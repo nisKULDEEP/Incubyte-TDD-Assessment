88bc5cc0f2729775529d9592d2f3bb46b3cb30b1
kuldeepnishad.singh <kuldeepnishad.singh@gmail.com>
2024-06-26 08:24:05 +0000

    stringCaluclator file created and basic UI created to insert input

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import add from './stringCalculator';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [string, setString] = useState<string>('');
+  const [result, setResult] = useState<number>(0);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <input type="text" value={string} onChange={(e) => setString(e.target.value)} />
+      <button onClick={() => {
+        const res:number = add(string);
+        setResult(res);
+        }}>Add</button>
+      {result && <p>{result}</p>}
+    </div>
     </>
   )
 }
---
+++ src/stringCalculator.ts
@@ -0,0 +1,8 @@
+import React from "react";
+
+const add = (number: string):number => {
+    return Number(number);
+
+}
+
+export default add;
